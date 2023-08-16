1681a62ef5a29e2dcf8d4c46ace9bcb943cc84b4
Dan Erik Petersen <dan@cloudcollab.nz>
2023-08-16 20:34:44 +1200

    fix: scale the background

--- code.js
+++ code.js
@@ -1,17 +1,28 @@
 /**
- * Scales all images on the page by the given ratio.
+ * Scales all images on the page by the given ratio, as well
+ * as the background on the body element.
  *
  * @param {number} ratio The ratio to scale the images by.
  * @return {void}
  */
 scaleImages = function(ratio) {
-    console.log('Scaling images by ratio: ' + ratio);
-
     if (ratio > 1) {
-        var images = document.getElementsByTagName('img');
+        console.log('Scaling images by ratio: ' + ratio);
+
+        // Scale all images on the page.
+        const images = document.getElementsByTagName('img');
         for (var i = 0; i < images.length; i++) {
             images[i].style.transform = 'scale(' + ratio + ')';
         }
+
+        // Determine the new background image size - we assume
+        // the background is a 2x2 image.
+        const sizeOfBackgroundImage = 2;
+        const backgroundSize = (ratio * sizeOfBackgroundImage).toFixed();
+
+        // Set the background size.
+        console.log('Scaling background image to ' + backgroundSize + 'px');
+        document.body.style.backgroundSize = backgroundSize + 'px';
     }
 };
 
